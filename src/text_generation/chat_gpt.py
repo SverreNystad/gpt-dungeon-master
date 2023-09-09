5b6dfe2a5553643e9d1d1879d990818297c5a5fd
Sverre Nystad <sverrnys@ntnu.no>
2023-09-09 23:15:19 +0200

    Feat: Make working implementation of chat_gpt that returns response

--- src/text_generation/chat_gpt.py
+++ src/text_generation/chat_gpt.py
@@ -1,5 +1,12 @@
+from enum import Enum
 import requests
 from config import GPTConfig
+
+
+class Role(Enum):
+    SYSTEM = "system"
+    ASSISTANT = "assistant"
+    USER = "user"
 
 def chat_with_gpt(prompt):
     # API endpoint (this is a hypothetical endpoint; refer to the official documentation for the exact endpoint)
@@ -14,17 +21,28 @@ def chat_with_gpt(prompt):
         "Authorization": f"Bearer {api_key}",
     }
     
+    history = [
+        { "role": Role.USER.value, "content": prompt },
+    ]
+    
     data = {
-        "prompt": prompt,
         "max_tokens": 150,
-        'model': GPTConfig().MODEL_NAME,
+        "model": GPTConfig().MODEL_NAME,
+        "temperature": 0.9,
+        "messages": history,
+        
     }
+
+    # "message": {
+    #   "role": "assistant",
+    #   "content": "\n\nHello there, how may I assist you today?",
     
     response = requests.post(endpoint, headers=headers, json=data)
     
     if response.status_code == 200:
         response_data = response.json()
-        return response_data["choices"][0]["text"].strip()
+        # return response_data["choices"][0]["text"]['content']
+        return response_data
     else:
         print("Error:", response.status_code)
         print("Error:", response.json())
@@ -36,3 +54,11 @@ if __name__ == "__main__":
     response = chat_with_gpt(prompt)
     if response:
         print("GPT Response:", response)
+
+
+"""GPTConfig
+Cosntruct message
+messages er en måte å sende meldinger
+den siste er prompten 
+role, content
+"""
