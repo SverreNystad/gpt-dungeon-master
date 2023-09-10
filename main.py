65c9b0db90670fe491adb3b750a5f835f5365698
Sverre Nystad <sverrnys@ntnu.no>
2023-09-10 23:08:24 +0200

    Feat: Add main entrance of running of code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from src.text_generation.chat_gpt import chat_with_gpt
 from src.image_generation.image_generation import generate_image_request, ImageSize
+
+# Run chat_gpt.py
+print("Running chat_gpt.py")
+prompt = input("Enter your prompt: ")
+gpt_response = chat_with_gpt(prompt)
+print(gpt_response)
 
 # Run image_generation.py
 print("Running image_generation.py")
-result = generate_image_request(prompt="A painting of a dungeon master sitting at a table with a group of adventurers playing Dungeons and Dragons.")
-print(result)
+image_result = generate_image_request(prompt="A painting of a dungeon master sitting at a table with a group of adventurers playing Dungeons and Dragons.")
+print(image_result)
 
