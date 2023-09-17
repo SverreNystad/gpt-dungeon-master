8a16295fdce822741cb9c404520b91609879814e
Sverre Nystad <sverrenystad@gmail.com>
2023-09-17 13:47:29 +0200

    Feat: Add standard Races to Race

--- src/non_player_character_generation.py
+++ src/non_player_character_generation.py
@@ -1,11 +1,29 @@
+from dataclasses import dataclass
 from langchain.llms import OpenAI
 from langchain.llms import OpenAI
+from enum import Enum
 
+class Race(Enum):
+    HUMAN = "human"
+    ELF = "elf"
+    DWARF = "dwarf"
+    HALF_ELF = "half elf"
+    DRAGONBORN = "dragonborn"
+    GNOME = "gnome"
+    HALF_ORC = "half orc"
+    TIEFLING = "tiefling"
+    HAFLING = "hafling"
+
+@dataclass()
 class NPC:
     """Class for non-player characters (NPCs)."""
-
-
-
+    
+    name: str
+    age: int
+    race: Race
+    occupation: str
+    
+    
 
 def generate_npc() -> NPC:
     """Generate an NPC."""
