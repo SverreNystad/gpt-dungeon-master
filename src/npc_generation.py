a5dc4680cb438cb74854463a52c102f22b25e24a
Sverre Nystad <sverrenystad@gmail.com>
2023-09-17 16:03:34 +0200

    Feat: Generate NPCs with NPC Profile, Create classes NPCRelations, NPCRelation, Create methods, generate_alignment, generate_general_background
    Co-authored-by: Knolaisen <66872548+Knolaisen@users.noreply.github.com>

--- src/npc_generation.py
+++ src/npc_generation.py
@@ -1,10 +1,15 @@
 from dataclasses import dataclass
+import random
 from langchain.llms import OpenAI
-# from langchain.chat_models import OpenAI
+from langchain.chat_models import ChatOpenAI
 from enum import Enum
 from src.text_generation.config import GPTConfig
 from langchain.schema import HumanMessage
 
+
+api_key = GPTConfig.API_KEY
+llm = OpenAI(openai_api_key=api_key)
+chat_model = ChatOpenAI(openai_api_key=api_key)
 
 
 class Race(Enum):
@@ -29,39 +34,90 @@ class Alignment(Enum):
     NEUTRAL_EVIL = "neutral evil"
     CHAOTIC_EVIL = "chaotic evil"
 
+@dataclass()
+class NPCProfile:
+    """The  for non-player characters (NPCs)."""
+    name: str
+    age: int
+    race: Race
+    alignment: Alignment
+    alive: bool
+    background: str
+
+@dataclass()
+class NPCRelations:
+    """The relations for non-player characters (NPCs)."""
+    list_of_relations: list[str]
+
+@dataclass()
+class NPCRelation:
+    name: str
+    type_of_relation: str
+    attitude: float
+    still_exist: bool
 
 @dataclass()
 class NPC:
     """Class for non-player characters (NPCs)."""
     # NPC PROFILE
-    name: str
-    age: int
-    race: Race
-    occupation: str
-    alignment: Alignment
+    NPCProfile: NPCProfile
     
+
+    # occupation: str
+    # relations: list[str] 
+
     # NPC STATS
 
 def generate_npc() -> NPC:
     """Generate an NPC."""
 
-    api_key = GPTConfig.API_KEY
-    llm = OpenAI(openai_api_key=api_key)
-    text = "What would be a good name for a elf wizard for a RPG?"
+    
 
     # messege = [HumanMessage(content=text)]
-    name: str = llm.predict(text)
     title: str = "The Wizard"
-    return name
+    age: int = 20
+    race: Race = Race.HALF_ELF
+    role: str = "Shopkeeper"
+
+    text = f"What would be a good name for a {race.value} that has the role of {role} for a RPG?"
+    name: str = llm.predict(text)
+
+    background = generate_general_background(name, age, race, role)
+    alignment = generate_alignment(background)
+
+    return NPC(NPCProfile(name, age, race, alignment, True, background))
+    
 
 
+def generate_alignment(info:str=None, alignment_list:list[Alignment]=[]) -> Alignment:
+    alignment_template = "What is the alignment of the NPC?"
+    if info is not None:
+        alignment_template += f" Based on this info: {info}."
+    else:
+        return random.choice(Alignment)
+    if len(alignment_list) == 0:
+        alignment_list = [alignment.value for alignment in Alignment]
+    alignment_template += f" It must be one of the following: {alignment_list}, do not give any other answer."
+    alignment = llm.predict(alignment_template)
+    print(alignment_template)
+    return alignment
+    if alignment not in alignment_list:
+        return generate_alignment(info, alignment_list)
 
-
+def generate_general_background(name: str, age: int, race: Race, role: str) -> str:
+    """Generate a background for an NPC."""
+    
+    text = f"""Generate a backstory for a NPC of race: {race.value}, with the name: {name}, and age: {age}. The character should have the role: {role} in the story.
+    """
+    return llm.predict(text)
+    
 
 def generate_dummy_npc() -> NPC:
     good_human = NPC("Rolf", 20, Race.HUMAN, "farmer", Alignment.NEUTRAL_GOOD)
 
     bad_half_orc = NPC("Mort", 40, Race.HALF_ORC, "bandit", Alignment.CHAOTIC_EVIL)
+    
+
     
     print(f"good_human {good_human}")
     print(f"bad_half_orc {bad_half_orc}")
