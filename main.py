48af5f6ef3dfbb7ca7658fdbeb2ac86ef8906aa9
Sverre Nystad <sverrenystad@gmail.com>
2023-09-18 01:18:39 +0200

    Feat: Integrate character creation with image generation

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from src.text_generation.chat_gpt import chat_with_gpt
+from src.npc_generation import Alignment, Race, generate_alignment, generate_general_background, generate_npc, generate_npc_relations
 from src.text_to_image.image_generator import generate_image_request, ImageSize
 
 # Run chat_gpt.py
-print("Running chat_gpt.py")
-prompt = input("Enter your prompt: ")
-gpt_response = chat_with_gpt(prompt)
-print(gpt_response)
+print("Running non_player_character_generation.py generating npc")
+npc = generate_npc()
+print(npc)
+
+
 
 # Run image_generation.py
 print("Running image_generation.py")
-image_result = generate_image_request(prompt="A painting of a dungeon master sitting at a table with a group of adventurers playing Dungeons and Dragons.")
+image_result = generate_image_request(prompt=npc.appearance, number_of_images=1, size=ImageSize(1024,1024))
 print(image_result)
 
