52be7868a2b8450365118ba5d10ff0be1cc64fe9
Sverre Nystad <sverrenystad@gmail.com>
2023-09-17 14:27:46 +0200

    Feat: Generate name and title for NPC, connect langchain
    Co-authored-by: Knolaisen <66872548+Knolaisen@users.noreply.github.com>

--- src/npc_generation.py
+++ src/npc_generation.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
 from langchain.llms import OpenAI
-from langchain.llms import OpenAI
+# from langchain.chat_models import OpenAI
 from enum import Enum
+from src.text_generation.config import GPTConfig
+from langchain.schema import HumanMessage
+
+
 
 class Race(Enum):
     HUMAN = "human"
@@ -24,18 +28,40 @@ class Alignment(Enum):
     LAWFUL_EVIL = "lawful evil"
     NEUTRAL_EVIL = "neutral evil"
     CHAOTIC_EVIL = "chaotic evil"
-    
+
+
 @dataclass()
 class NPC:
     """Class for non-player characters (NPCs)."""
-    
+    # NPC PROFILE
     name: str
     age: int
     race: Race
     occupation: str
     alignment: Alignment
     
+    # NPC STATS
 
 def generate_npc() -> NPC:
     """Generate an NPC."""
-    pass
+
+    api_key = GPTConfig.API_KEY
+    llm = OpenAI(openai_api_key=api_key)
+    text = "What would be a good name for a elf wizard for a RPG?"
+
+    # messege = [HumanMessage(content=text)]
+    name: str = llm.predict(text)
+    title: str = "The Wizard"
+    return name
+
+
+
+
+
+def generate_dummy_npc() -> NPC:
+    good_human = NPC("Rolf", 20, Race.HUMAN, "farmer", Alignment.NEUTRAL_GOOD)
+
+    bad_half_orc = NPC("Mort", 40, Race.HALF_ORC, "bandit", Alignment.CHAOTIC_EVIL)
+    
+    print(f"good_human {good_human}")
+    print(f"bad_half_orc {bad_half_orc}")
