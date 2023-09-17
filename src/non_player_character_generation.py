e81acd329ab150e4fd9a2d2ae1d2617f422a555a
Sverre Nystad <sverrenystad@gmail.com>
2023-09-17 13:39:11 +0200

    Feat: Create NPC class
    Co-authored-by: Knolaisen <Knolaisen@users.noreply.github.com>

---
+++ src/non_player_character_generation.py
@@ -0,0 +1,12 @@
+from langchain.llms import OpenAI
+from langchain.llms import OpenAI
+
+class NPC:
+    """Class for non-player characters (NPCs)."""
+
+
+
+
+def generate_npc() -> NPC:
+    """Generate an NPC."""
+    pass
