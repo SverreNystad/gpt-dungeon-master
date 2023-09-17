2100f174e69c5bbdd56d16ea744e37a9f535a729
Sverre Nystad <sverrenystad@gmail.com>
2023-09-17 13:48:34 +0200

    Feat: Add Alignment

--- src/non_player_character_generation.py
+++ src/non_player_character_generation.py
@@ -14,6 +14,17 @@ class Race(Enum):
     TIEFLING = "tiefling"
     HAFLING = "hafling"
 
+class Alignment(Enum):
+    LAWFUL_GOOD = "lawful good"
+    NEUTRAL_GOOD = "neutral good"
+    CHAOTIC_GOOD = "chaotic good"
+    LAWFUL_NEUTRAL = "lawful neutral"
+    NEUTRAL = "neutral"
+    CHAOTIC_NEUTRAL = "chaotic neutral"
+    LAWFUL_EVIL = "lawful evil"
+    NEUTRAL_EVIL = "neutral evil"
+    CHAOTIC_EVIL = "chaotic evil"
+    
 @dataclass()
 class NPC:
     """Class for non-player characters (NPCs)."""
@@ -22,7 +33,7 @@ class NPC:
     age: int
     race: Race
     occupation: str
-    
+    alignment: Alignment
     
 
 def generate_npc() -> NPC:
