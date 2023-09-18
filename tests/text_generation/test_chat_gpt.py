5f34c03f11f0e996ec445039cae12b9efa03f51a
Sverre Nystad <sverrenystad@gmail.com>
2023-09-19 00:17:28 +0200

    Fix: GPT test

--- tests/text_generation/test_chat_gpt.py
+++ tests/text_generation/test_chat_gpt.py
@@ -1,8 +1,8 @@
 import pytest
-# import src.text_generation.chat_gpt as chat_gpt
+import src.text_generation.chat_gpt as chat_gpt
 
+@pytest.mark.apitest
 def test_chat_gpt_response():
-    # prompt = "Say anything!"
-    # response = chat_gpt.chat_with_gpt(prompt)
-    # assert response
-    assert True
+    prompt = "Say anything!"
+    response = chat_gpt.chat_with_gpt(prompt)
+    assert response
