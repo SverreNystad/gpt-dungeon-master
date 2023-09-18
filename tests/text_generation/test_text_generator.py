0b906be0e08d682e31eb95ffaf3471fc9b77a4d4
Sverre Nystad <sverrenystad@gmail.com>
2023-09-19 01:57:37 +0200

    Feat: Add tests

--- tests/text_generation/test_text_generator.py
+++ tests/text_generation/test_text_generator.py
@@ -1,6 +1,20 @@
 import pytest
 from src.text_generation.text_generator import get_default_text_generator, TextGenerator, LLM, Chatbot
 
+def test_get_default_text_generator():
+    generator: TextGenerator = get_default_text_generator()
+    assert generator is not None, "The generator was None"
+    assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
+
+def test_LLM():
+    generator: TextGenerator = LLM()
+    assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
+
+def test_Chatbot():
+    generator: TextGenerator = Chatbot()
+    assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
+
+
 @pytest.mark.apitest
 def test_text_generator():
     prompt = "Say anything!"
