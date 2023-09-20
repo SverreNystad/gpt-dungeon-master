2c76f3446b9d18b5c23ee34f9fc6ed6737f01ede
Sverre Nystad <sverrenystad@gmail.com>
2023-09-20 23:41:49 +0200

    Feat: Add GMA

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import logging
-from src.text_generation.chat_gpt import chat_with_gpt
 from src.npc_generation import Alignment, Race, generate_alignment, generate_general_background, generate_npc, generate_npc_relations
 from src.text_to_image.image_generator import generate_image_request, ImageSize
+from src.agents.dungeon_master import run_dungeon_master
 
 # Set up logging
 logging.basicConfig(filename='gpt_dungeon_master.log',
-                    level=logging.INFO,
+                    level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Create a logger instance for this script
 logger = logging.getLogger(__name__)
 
-# Run chat_gpt.py
-logger.info("Running non_player_character_generation.py generating npc")
-npc = generate_npc()
-logger.info(npc)
 
-# Run image_generation.py
-logger.info("Running image_generation.py")
-image_result = generate_image_request(prompt=npc.appearance, number_of_images=1, size=ImageSize(1024,1024))
-logger.info(image_result)
+# Run dungeon master agent
+logger.info("Running dungeon_master.py")
+while True:
+    prompt = input("The GPT Dungeon Master awaits your input: ")
+
+    answer = run_dungeon_master(prompt=prompt)
+    print(answer)
+    logger.info(f"Prompt: {prompt}, Answer: {answer}")
+
+logger.info("Finished running dungeon_master.py")
--- src/agents/dungeon_master.py
+++ src/agents/dungeon_master.py
@@ -11,25 +11,64 @@ from src.text_generation.text_generator import get_default_text_generator
 # Set up logging
 logger = logging.getLogger(__name__)
 
+
+def get_dungeon_master_template():
+    """
+    Return the dungeon master template.
+    """
+    dungeon_master_template = """
+    You shall act as the narrator of the story. 
+    You are in charge of the game world and the NPCs that inhabit it.
+    You are also in charge of the rules of the game and the challenges that the players face.
+    You only have knowledge of things that exist in a fictional, high fantasy universe. 
+    You must not break character under any circumstances.
+    Keep responses under 500 words. 
+    Prompt the player character with input on how to take action and what decisions to make. 
+    Do not make decisions for the player character.
+    """
+
+    return dungeon_master_template
+
+def narrate(prompt: str) -> str:
+    """
+    Narrate the story based on the given prompt.
+    """
+    template = get_dungeon_master_template()
+    generator = get_default_text_generator()
+    narration = generator.predict(template + prompt)
+    print(narration)
+    return narration
+
 tools = [
     StructuredTool.from_function(
         name= "NPC Generator", 
         func=generate_npc, 
         description="Generates a NPC based on the given prompt."
     ),
+    # StructuredTool.from_function(
+    #     name = "Difficulty Analyzer",
+    #     func=decide_difficulty,
+    #     description="Decides the difficulty of the challenge based on the context. Values between 0 and 1, where 0 is trivial and 1 is nearly impossible."
+    # ),
     StructuredTool.from_function(
-        name = "Difficulty Analyzer",
-        func=decide_difficulty,
-        description="Decides the difficulty of the challenge based on the context. Values between 0 and 1, where 0 is trivial and 1 is nearly impossible."
+        name = "Narrator",
+        func=narrate,
+        description="Narrates the story based on the given prompt."
     ),
-    
 ]
 
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 # llm = get_default_text_generator(temperature=0.7, is_llm=False)
 llm=OpenAI(temperature=0)
-agent_chain = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True, memory=memory)
+agent_chain = initialize_agent(
+    tools, 
+    llm, 
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, 
+    verbose=False, 
+    memory=memory,
+    max_iterations=1,
+    )
 
 def run_dungeon_master(prompt) -> str:
     """Run the dungeon master agent."""    
