08798c083e1e4489b21f42ab8a8fe38eed1e8063
Sverre Nystad <sverrenystad@gmail.com>
2023-09-20 19:42:29 +0200

    Add Difficulty Analyzer tool and implement run_dungeon_master function
    - Add Difficulty Analyzer tool to the dungeon_master.py file to decide the difficulty of the challenge based on the context.
    - Implement the run_dungeon_master function to run the dungeon master agent with the given prompt.
    - The run_dungeon_master function checks the validity of the prompt length and returns the result of the agent run.
    
    Note: The descriptions are slightly longer than 50 characters, but they will be truncated in the commit message.

--- src/agents/dungeon_master.py
+++ src/agents/dungeon_master.py
@@ -1,10 +1,10 @@
 from langchain.agents import Tool
 from langchain.agents import AgentType
 from langchain.memory import ConversationBufferMemory
-from langchain.llms import OpenAI
 from langchain.agents import initialize_agent
 import logging
 from src.npc_generation import generate_npc
+from src.referee import decide_difficulty
 from src.text_generation.text_generator import get_default_text_generator
 
 # Set up logging
@@ -16,6 +16,11 @@ tools = [
         func=generate_npc(),
         description="Generates a NPC based on the given prompt."
     ),
+    Tool(
+        name = "Difficulty Analyzer",
+        func=decide_difficulty(),
+        description="Decides the difficulty of the challenge based on the context."
+    ),
 ]
 
 
@@ -26,3 +31,13 @@ llm = get_default_text_generator(temperature=0.7, is_llm=False)
 
 agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 
+def run_dungeon_master(prompt) -> str:
+    """Run the dungeon master agent."""
+    logger.info("Running dungeon_master.py")
+    assert isinstance(prompt, str), "Prompt must be a string."
+    if (len(prompt) < 1) or (len(prompt) > 1000):
+        raise ValueError("Prompt must be at least 1 character or less than 1000 characters.")
+    
+    dm_result = agent_chain.run(prompt)
+    logger.info(f"Finished running dungeon_master.py, result: {dm_result}")
+    return dm_result
