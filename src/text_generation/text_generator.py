6f79401da1651cbce5d4132867dd50bafde87d93
Sverre Nystad <sverrenystad@gmail.com>
2023-09-20 19:06:00 +0200

    Feat: Update TextGenerator and Chatbot constructors to include a temperature parameter
    - Added a temperature parameter to the TextGenerator and Chatbot constructors to allow customization of text generation.
    - Modified the `__init__` method of LLM to include the temperature parameter.
    - Modified the `__init__` method of Chatbot to include the temperature parameter.
    - Updated the `get_default_text_generator` function to accept a temperature parameter and added assertions to validate the input.
    - Updated the tests to include new test cases for the `get_default_text_generator` function.

--- src/text_generation/text_generator.py
+++ src/text_generation/text_generator.py
@@ -23,9 +23,9 @@ class TextGenerator(ABC):
 class LLM(TextGenerator):
     """A text generator that uses the Language Model API from OpenAI."""
 
-    def __init__(self, api_key: str=None):
+    def __init__(self, api_key: str=None, temperature: float=0.0):
         self.__api_key = GPTConfig.API_KEY if api_key is None else api_key
-        self.__llm = OpenAI(openai_api_key=self.__api_key)
+        self.__llm = OpenAI(openai_api_key=self.__api_key, temperature=temperature)
 
     def predict(self, prompt: str) -> str:
         """Predict the next word based on the prompt."""
@@ -34,16 +34,27 @@ class LLM(TextGenerator):
 class Chatbot(TextGenerator):
     """A chatbot that can chat with a user."""
 
-    def __init__(self, api_key: str=None):
+    def __init__(self, api_key: str=None, temperature: float=0.7):
         self.__api_key = GPTConfig.API_KEY if api_key is None else api_key
-        self.__chatbot = ChatOpenAI(openai_api_key=self.__api_key)
+        self.__chatbot = ChatOpenAI(openai_api_key=self.__api_key, temperature=temperature)
 
 
     def predict(self, prompt: str) -> str:
         """Chat with the chatbot."""
         return self.__chatbot.predict(prompt)
 
-def get_default_text_generator() -> TextGenerator:
-    """Return the default text generator."""
-    api_key = GPTConfig.API_KEY
-    return LLM(api_key=api_key)
+def get_default_text_generator(temperature: float = 0.7, is_llm: bool = True) -> TextGenerator:
+    """Return the default text generator.
+    Args:
+        temperatur (float): The temperature of the text generation. Must be between 0.0 and 1.0. 
+        It is focused on the most likely tokens when set to 0.0 and focused on the most creative tokens when set to 1.0.
+        is_llm (bool): Whether to use the LLM or the Chatbot. True for LLM, False for Chatbot.
+    Returns:
+        :return: A text generator.
+    """
+    assert 0.0 <= temperature <= 1.0, "Temperature must be between 0.0 and 1.0"
+    if is_llm:
+        return LLM(temperature)
+    else:
+        return Chatbot(temperature)
+
--- tests/text_generation/test_text_generator.py
+++ tests/text_generation/test_text_generator.py
@@ -15,6 +15,24 @@ def test_Chatbot():
     generator: TextGenerator = Chatbot("DUMMY_KEY")
     assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
 
+def get_default_text_generator():
+    # Arrange
+    get_llm = True
+    # Act
+    generator: TextGenerator = get_default_text_generator(is_llm=get_llm)
+    # Assert
+    assert generator is not None, "The generator was None"
+    assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
+
+def get_default_text_generator_chatbot():
+    # Arrange
+    get_llm = False
+    # Act
+    generator: TextGenerator = get_default_text_generator(is_llm=get_llm)
+    # Assert
+    assert generator is not None, "The generator was None"
+    assert isinstance(generator, TextGenerator), "The generator was not an TextGenerator"
+
 
 @pytest.mark.apitest
 def test_text_generator():
