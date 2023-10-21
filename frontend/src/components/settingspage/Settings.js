5d96ae7ce72e1af7b2ddb7df402c8bc8c7051112
Sverre Nystad <sverrenystad@gmail.com>
2023-10-21 04:01:07 +0200

    feat: Add state variables for resolution and language settings to SettingsPage
    This commit adds state variables for resolution and language settings to the SettingsPage component. The default resolution is set to '1920x1080' and the default language is set to 'en'. Additionally, the resetToDefaults function now includes resetting these new settings to their defaults.

--- frontend/src/components/settingspage/Settings.js
+++ frontend/src/components/settingspage/Settings.js
@@ -3,12 +3,19 @@ import React, { useState } from 'react';
 const SettingsPage = () => {
     // You might have state variables to track each setting, which could potentially be fetched from a server or local storage
     const [backgroundVolume, setBackgroundVolume] = useState(0.5); // assuming 0.5 is the default value
-    // ... other state variables
+    const [gptSpeech, setGptSpeech] = useState(0.5); // assuming 0.5 is the default value
     
+    // State variable for resolution setting
+    const [resolution, setResolution] = useState('1920x1080'); // default value
+    
+    // State variable for language setting
+    const [language, setLanguage] = useState('en'); // default value is English
+
     const resetToDefaults = () => {
-        // Reset all values to their defaults
         setBackgroundVolume(0.5);
-        // ... reset other settings
+        setGptSpeech(0.5);
+        setResolution('1920x1080');
+        setLanguage('en');
     };
 
     return (
@@ -27,6 +34,15 @@ const SettingsPage = () => {
                         value={backgroundVolume} 
                         onChange={(e) => setBackgroundVolume(e.target.value)} 
                     />
+
+                    <label>DM Speech Volume</label>
+                    <input 
+                        type="range" 
+                        min="0" max="1" 
+                        step="0.01" 
+                        value={gptSpeech} 
+                        onChange={(e) => setGptSpeech(e.target.value)} 
+                    />
                     {/* Other audio controls go here */}
                 </div>
             </section>
@@ -34,13 +50,29 @@ const SettingsPage = () => {
             {/* Display Settings */}
             <section>
                 <h2>Display Settings</h2>
-                {/* Display controls go here */}
+                <div>
+                    <label>Screen Resolution</label>
+                    <select value={resolution} onChange={(e) => setResolution(e.target.value)}>
+                        <option value="1920x1080">1920x1080</option>
+                        <option value="1280x720">1280x720</option>
+                        <option value="800x600">800x600</option>
+                        {/* Add as many resolutions as you need */}
+                    </select>
+                </div>
             </section>
 
             {/* Language and Subtitles */}
             <section>
                 <h2>Language and Subtitles</h2>
-                {/* Language controls go here */}
+                <div>
+                    <label>Language</label>
+                    <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+                        <option value="en">English</option>
+                        <option value="es">Spanish</option>
+                        <option value="de">German</option>
+                        {/* Add as many languages as you support */}
+                    </select>
+                </div>
             </section>
 
             {/* Reset Options */}
