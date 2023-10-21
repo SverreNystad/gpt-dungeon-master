b4e1d8a41ecaeca5223896db6e6b600f1dbfd5a6
Sverre Nystad <sverrenystad@gmail.com>
2023-10-21 04:24:52 +0200

    Feat: Check that the context work on other pages

--- frontend/src/components/mainmenu/MainMenu.jsx
+++ frontend/src/components/mainmenu/MainMenu.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react'; // <-- useContext is imported here
 import { Link } from 'react-router-dom';
 import './MainMenu.css';
 
+// import React, { useContext  } from 'react';
+import { SettingsContext } from '../../contexts/SettingsContext'; // adjust the import path as needed
+
 function MainMenu() {
+  const { settings } = useContext(SettingsContext); // <-- Using useContext here to consume the context
+
+  console.log("MainMenu");
+  console.log(settings);
   return (
     <div className="main-menu">
       <Link to="/new-campaign"><button>Start New Campaign</button></Link>
