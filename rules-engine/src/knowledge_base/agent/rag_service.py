1eba055b46ba155d4ad1322ed6901e57a2c7a4b0
Jon Bergland <jobergl@stud.ntnu.no>
2025-01-08 13:02:58 +0100

    feat: add method for looking up rules
    Co-authored-by: Sverre Nystad SverreNystad@users.noreply.github.com

--- rules-engine/src/knowledge_base/agent/rag_service.py
+++ rules-engine/src/knowledge_base/agent/rag_service.py
@@ -33,3 +33,44 @@ def lookup_rules(context, filepath = "knowledge_base/rulesystems/rules.txt") -> 
     result = chain.invoke({"rules_document":rules_document, "context":context})
     rules = result
     return rules
+
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def lookup_rules_v2(context, filepath = "knowledge_base/rulesystems/rules.txt") -> list[str]:
+    rules_document = ""
+
+    with open(filepath, encoding="utf-8") as f:
+        rules_document = f.read()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(rules_document)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    retriever = vectorstore.as_retriever()
+    prompt = PromptTemplate(
+        template="""Given the following document providing the rules of the role playing system: '''{rules_document}''' Give a list of relevant rules in json based on the context provided: '''{context}''' Only include the json rules.""", 
+        input_variables=["rules_document", "context"]
+    )
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+
+    rag_chain = (
+        prompt
+        | retriever
+        | RunnablePassthrough(format_docs)
+        | Agent.model
+        | StrOutputParser()
+    )
+
+    result = rag_chain.invoke({"context": context})
+    rules = result
+    return rules
+
