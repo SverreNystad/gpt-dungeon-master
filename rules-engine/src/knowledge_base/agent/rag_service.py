0061cda7ca0f0ab63d2e0b45ae9de2c2a157e8e2
Jon Bergland <jobergl@stud.ntnu.no>
2025-01-09 23:33:36 +0100

    feat: add RagService class
    Co-authored-by: Sverre Nystad SverreNystad@users.noreply.github.com

--- rules-engine/src/knowledge_base/agent/rag_service.py
+++ rules-engine/src/knowledge_base/agent/rag_service.py
@@ -1,120 +1,133 @@
 from knowledge_base.agent.agent import Agent
 from knowledge_base.agent.rag import RAG
-
-from langchain_core.prompts import PromptTemplate
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-# LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY", "no_key")
-# LANGCHAIN_TRACING_V2=True
-# LANGCHAIN_ENDPOINT="https://eu.api.smith.langchain.com"
-# LANGCHAIN_PROJECT="GPT-gm"
-
-
-
-def lookup_rules(context, filepath = "knowledge_base/rulesystems/rules.txt") -> list[str]:
-    """
-    Lookup rules based on the context
-    """
-    rules_document = ""
-
-    with open(filepath, encoding="utf-8") as f:
-        rules_document = f.read()
-    
-    MAX_LENGTH = 128_000
-
-    if len(rules_document) > MAX_LENGTH:
-        rules_document = rules_document[:MAX_LENGTH]
-    rules = []
-    prompt = PromptTemplate(
-        template="""Given the following document providing the rules of the role playing system: '''{rules_document}''' Give a list of relevant rules in json based on the context provided: '''{context}''' Only include the json rules.""", 
-        input_variables=["rules_document", "context"]
-    )
-
-    
-    chain = prompt | Agent.model
-    result = chain.invoke({"rules_document":rules_document, "context":context})
-    rules = result
-    return rules
-
-from langchain import hub
+from ragas import EvaluationDataset
+from langchain_openai import OpenAIEmbeddings
+from knowledge_base.models.models import OpenAIModels
 from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def lookup_rules_v2(context, filepath = "knowledge_base/rulesystems/rules.txt") -> list[str]:
-    rules_document = ""
-
-    with open(filepath, encoding="utf-8") as f:
-        rules_document = f.read()
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(rules_document)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-    # Retrieve and generate using the relevant snippets of the blog.
-    retriever = vectorstore.as_retriever()
-    prompt = PromptTemplate(
-        template="""Given the following document providing the rules of the role playing system: '''{rules_document}''' Give a list of relevant rules in json based on the context provided: '''{context}''' Only include the json rules.""", 
-        input_variables=["rules_document", "context"]
-    )
-
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
+from langchain_core.prompts import PromptTemplate
+import pandas as ps
 
 
-    rag_chain = (
-        prompt
-        | retriever
-        | RunnablePassthrough(format_docs)
-        | Agent.model
-        | StrOutputParser()
-    )
+class RagService:
+    def __init__(self):
+        self.rag = RAG(OpenAIModels.gpt_4o_mini)
 
-    result = rag_chain.invoke({"context": context})
-    rules = result
-    return rules
+        filepath = "knowledge_base/rulesystems/cc-srd5.md"
 
+        rules_document: str = ""
+        with open(filepath, encoding="utf-8") as f:
+            rules_document = f.read()
 
-def rag_setup():
-    from ragas.llms import LangchainLLMWrapper
-    from ragas.embeddings import LangchainEmbeddingsWrapper
-    from langchain_openai import ChatOpenAI
-    from langchain_openai import OpenAIEmbeddings
-    from knowledge_base.models.models import OpenAIModels
-    from langchain_core.documents import Document
-    evaluator_llm = LangchainLLMWrapper(OpenAIModels.gpt_4o_mini)
-    evaluator_embeddings = LangchainEmbeddingsWrapper(OpenAIEmbeddings())
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_text(rules_document)
+        
+        # Load documents
+        self.rag.load_documents(splits)
 
-    # filepath = "rules-engine/src/knowledge_base/rulesystems/cc-srd5.md"
-    filepath = "knowledge_base/rulesystems/cc-srd5.md"
+    def lookup_rule(self, context, filepath = "knowledge_base/rulesystems/rules.txt") -> list[str]:
+        rules_document = ""
 
-    rules_document: str = ""
-    with open(filepath, encoding="utf-8") as f:
-        rules_document = f.read()
-    
+        with open(filepath, encoding="utf-8") as f:
+            rules_document = f.read()
 
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_documents(rules_document)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_text(rules_document)
+        # Retrieve and generate using the relevant snippets of the blog.
+        retriever = vectorstore.as_retriever()
+        prompt = PromptTemplate(
+            template="""Given the following document providing the rules of the role playing system: '''{rules_document}''' Give a list of relevant rules in json based on the context provided: '''{context}''' Only include the json rules.""", 
+            input_variables=["rules_document", "context"]
+        )
 
-
-    rag = RAG(OpenAIModels.gpt_4o_mini)
-
-    # Load documents
-    rag.load_documents(splits)
-
-    # Query and retrieve the most relevant document
-    query = "How many hitpoints does an Barbarian have at level 1? "
-    relevant_doc = rag.get_most_relevant_docs(query)
-
-    # Generate an answer
-    answer = rag.generate_answer(query, relevant_doc)
-
-    print(f"Query: {query}")
-    print(f"Relevant Document: {relevant_doc}")
-    print(f"Answer: {answer}")
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
 
 
+        rag_chain = (
+            prompt
+            | retriever
+            | RunnablePassthrough(format_docs)
+            | Agent.model
+            | StrOutputParser()
+        )
+        result = rag_chain.invoke({"context": context})
+        rules = result
+        return rules
+
+
+    def rag_setup():
+
+        # filepath = "rules-engine/src/knowledge_base/rulesystems/cc-srd5.md"
+        filepath = "knowledge_base/rulesystems/cc-srd5.md"
+
+        rules_document: str = ""
+        with open(filepath, encoding="utf-8") as f:
+            rules_document = f.read()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        splits = text_splitter.split_text(rules_document)
+
+        rag = RAG(OpenAIModels.gpt_4o_mini)
+
+        # Load documents
+        rag.load_documents(splits)
+
+        # Query and retrieve the most relevant document
+        query = "How many hitpoints does an Barbarian have at level 1? "
+        relevant_doc = rag.get_most_relevant_docs(query)
+
+        # Generate an answer
+        answer = rag.generate_answer(query, relevant_doc)
+
+        print(f"Query: {query}")
+        print(f"Relevant Document: {relevant_doc}")
+        print(f"Answer: {answer}")
+
+
+    def rag_evaluator(self):
+        from ragas import evaluate
+        from ragas.llms import LangchainLLMWrapper
+        from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
+
+
+        dataset = []
+
+        df = ps.read_csv("knowledge_base/validation_data/sample_questions_cl.csv")
+        querys = df["question"].tolist()
+        responses = df["answer"].tolist()
+
+
+        for query, reference in zip(querys, responses):
+
+            relevant_docs = self.rag.get_most_relevant_docs(query, 5, 0.70)
+            response = self.rag.generate_answer(query, relevant_docs)
+            dataset.append(
+                {
+                    "user_input":query,
+                    "retrieved_contexts":relevant_docs,
+                    "response":response,
+                    "reference":reference
+                }
+            )
+
+        print(dataset)
+        evaluation_dataset = EvaluationDataset.from_list(dataset)
+
+        evaluator_llm = LangchainLLMWrapper(Agent.model)
+        
+
+        result = evaluate(
+            dataset=evaluation_dataset,
+            metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()],
+            llm=evaluator_llm
+        )
+        return result
 
 
