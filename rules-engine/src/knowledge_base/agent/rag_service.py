9c9abd149aae4c0a67ce26c9ff28ca49dcdce033
Jon Bergland <jobergl@stud.ntnu.no>
2025-01-27 21:48:29 +0100

    refactor: remove unused code

--- rules-engine/src/knowledge_base/agent/rag_service.py
+++ rules-engine/src/knowledge_base/agent/rag_service.py
@@ -12,9 +12,26 @@ from langchain_experimental.text_splitter import SemanticChunker
 from langchain.retrievers import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain_core.documents import Document
+from langchain_core.stores import BaseStore
+from ragas import evaluate
+from ragas.llms import LangchainLLMWrapper
+from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
+
 
 from langchain_core.prompts import PromptTemplate
 import pandas as ps
+
+def fetch_store(splits: list[Document]) -> BaseStore:
+    store: BaseStore = InMemoryStore()
+
+    # Check for values are in persistance
+
+    for i, doc in enumerate(splits):
+        store.mset([(i, doc)])
+
+    # Retrieve data from file 
+    return store
+    
 
 
 class RagService:
@@ -22,76 +39,7 @@ class RagService:
         embedding_model = "text-embedding-3-small"
         self.rag = RAG(OpenAIModels.gpt_4o_mini, embeddings_model=embedding_model)
 
-        filepath = "knowledge_base/rulesystems/cc-srd5.md"
-
-        rules_document: str = ""
-        with open(filepath, encoding="utf-8") as f:
-            rules_document = f.read()
-
-
-        docs = [Document(rules_document)]
-
-        headers_to_split_on = [
-            ("#", "Header 1"),
-            ("##", "Header 2")
-        ]
-
-        markdown_splitter = MarkdownTextSplitter()
-        markdown_splitter._separators = self.get_md_seperators()
-        #markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on, strip_headers=False)
-        splits = markdown_splitter.split_documents(docs)
-        print(len(splits))
-        
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=400,chunk_overlap = 200)
-        #splits = text_splitter.split_documents(md_splits)
-
-
-        # text_splitter = SemanticChunker(
-        #     OpenAIEmbeddings(), breakpoint_threshold_type="percentile"
-        # )
-
-        #splits: list[str] = []
-        # for doc in text_splitter.split_documents(md_splits):
-        #     splits.append(doc.page_content)
-
-        store = InMemoryStore()
-        vectorstore = Chroma(embedding_function=self.rag.embeddings, collection_name="rules_doc", persist_directory="knowledge_base/db_data")
-        
-        self.parent_doc_retriever = ParentDocumentRetriever(
-            vectorstore=vectorstore,
-            docstore=store,
-            child_splitter=text_splitter,
-            parent_splitter =markdown_splitter
-        )
-
-        batch_size = 5
-        splits_length = len(splits)
-        print(f"Docs length: {splits_length}")
-        for i in range(0, splits_length, batch_size):
-            print(f"Batch: {i}")
-            ids = []
-            if (i + batch_size < splits_length):
-                batch = splits[i:i+batch_size]
-                for k in range(i, i + batch_size):
-                    ids.append(k)
-            else:
-                batch = splits[i:splits_length]
-                for k in range(i, splits_length):
-                    ids.append(k)
-
-            self.parent_doc_retriever.add_documents(batch, ids=ids)
-
-        print(f"Number of parent chunks  is: {len(list(store.yield_keys()))}")
-
-        print(f"Number of child chunks is: {len(self.parent_doc_retriever.vectorstore.get()['ids'])}")
-
-        
-        print("Finished adding docs")
-
-        #self.parent_doc_retriever.add_documents(splits, ids=None)
-
-        # Load documents
-        #self.rag.load_documents(splits)
+        self.rag_setup()
 
     def get_md_seperators(self):
         return [
@@ -113,98 +61,65 @@ class RagService:
             " ",
             "",
         ]
-    
-    def generate_Chroma_database():
-        return 0
 
-    def lookup_rule(self, context, filepath = "knowledge_base/rulesystems/rules.txt") -> list[str]:
-        rules_document = ""
-
-        with open(filepath, encoding="utf-8") as f:
-            rules_document = f.read()
-
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        splits = text_splitter.split_documents(rules_document)
-        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-        # Retrieve and generate using the relevant snippets of the blog.
-        retriever = vectorstore.as_retriever()
-        prompt = PromptTemplate(
-            template="""Given the following document providing the rules of the role playing system: '''{rules_document}''' Give a list of relevant rules in json based on the context provided: '''{context}''' Only include the json rules.""", 
-            input_variables=["rules_document", "context"]
-        )
-
-        def format_docs(docs):
-            return "\n\n".join(doc.page_content for doc in docs)
-
-
-        rag_chain = (
-            prompt
-            | retriever
-            | RunnablePassthrough(format_docs)
-            | Agent.model
-            | StrOutputParser()
-        )
-        result = rag_chain.invoke({"context": context})
-        rules = result
-        return rules
-
-
-    def rag_setup():
-
-        # filepath = "rules-engine/src/knowledge_base/rulesystems/cc-srd5.md"
+    def rag_setup(self):
         filepath = "knowledge_base/rulesystems/cc-srd5.md"
 
         rules_document: str = ""
         with open(filepath, encoding="utf-8") as f:
             rules_document = f.read()
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        splits = text_splitter.split_text(rules_document)
+        docs = [Document(rules_document)]
 
+        markdown_splitter = MarkdownTextSplitter()
+        markdown_splitter._separators = self.get_md_seperators()
+        splits = markdown_splitter.split_documents(docs)
+        print(len(splits))
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=400,chunk_overlap = 200)
 
-        rag = RAG(OpenAIModels.gpt_4o_mini)
+        store = fetch_store(splits)
+        vectorstore = Chroma(embedding_function=self.rag.embeddings, collection_name="rules_doc", persist_directory="knowledge_base/db_data")
+        
+        self.parent_doc_retriever = ParentDocumentRetriever(
+            vectorstore=vectorstore,
+            docstore=store,
+            child_splitter=text_splitter,
+            parent_splitter=markdown_splitter
+        )
+        if self.parent_doc_retriever.docstore.mget(["0"]) == None:
+            batch_size = 5
+            splits_length = len(splits)
+            print(f"Docs length: {splits_length}")
+            for i in range(0, splits_length, batch_size):
+                print(f"Batch: {i}")
+                ids = []
+                if (i + batch_size < splits_length):
+                    batch = splits[i:i+batch_size]
+                    for k in range(i, i + batch_size):
+                        ids.append(k)
+                else:
+                    batch = splits[i:splits_length]
+                    for k in range(i, splits_length):
+                        ids.append(k)
 
-        # Load documents
-        rag.load_documents(md_splits)
+                self.parent_doc_retriever.add_documents(batch, ids=ids)
 
-        # Query and retrieve the most relevant document
-        query = "How many hitpoints does an Barbarian have at level 1? "
-        relevant_doc = rag.get_most_relevant_docs(query)
+                #TODO Save to file 
 
-        # Generate an answer
-        answer = rag.generate_answer(query, relevant_doc)
+        print(f"Number of parent chunks  is: {len(list(store.yield_keys()))}")
 
-        print(f"Query: {query}")
-        print(f"Relevant Document: {relevant_doc}")
-        print(f"Answer: {answer}")
+        print(f"Number of child chunks is: {len(self.parent_doc_retriever.vectorstore.get()['ids'])}")
+
+        print("Finished adding docs")
 
 
     def rag_evaluator(self):
-        from ragas import evaluate
-        from ragas.llms import LangchainLLMWrapper
-        from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
-
-
         dataset = []
 
-        df = ps.read_csv("knowledge_base/validation_data/sample_questions_race.csv")
+        df = ps.read_csv("knowledge_base/validation_data/sample_questions_class.csv")
         querys = df["question"].tolist()
         responses = df["answer"].tolist()
-
-
-        # for query, reference in zip(querys, responses):
-
-        #     relevant_docs = self.rag.get_most_relevant_docs(query, 5, 0.70)
-        #     response = self.rag.generate_answer(query, relevant_docs)
-        #     dataset.append(
-        #         {
-        #             "user_input":query,
-        #             "retrieved_contexts":relevant_docs,
-        #             "response":response,
-        #             "reference":reference
-        #         }
-        #     )
 
         for query, reference in zip(querys, responses):
 
@@ -224,7 +139,6 @@ class RagService:
 
         evaluator_llm = LangchainLLMWrapper(Agent.model)
         
-
         result = evaluate(
             dataset=evaluation_dataset,
             metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()],
