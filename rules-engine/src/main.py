4db61917ded38a39fa08dd9a5ca5cf3535f560a9
Jon Bergland <jobergl@stud.ntnu.no>
2025-02-02 16:37:03 +0100

    feat: optuna functionality for Parent_Document retriever

--- rules-engine/src/main.py
+++ rules-engine/src/main.py
@@ -4,6 +4,9 @@ import optuna
 import optunahub
 from optuna.study import StudyDirection
 from optuna_dashboard import run_server
+import logging
+import sys
+
 
 def test_rules_lookup():
     description = "You are in a dark cave, and you hear a loud growl coming from the darkness"
@@ -123,16 +126,45 @@ Your draconic heritage manifests in a variety of traits you share with other dra
     scorer = Faithfulness()
     await scorer.single_turn_ascore(sample)
 
-def objective(trial: optuna.Trial) -> float:
+def objective2(trial: optuna.Trial) -> float:
     nr_md_splitts = trial.suggest_int("md_splits", 1, 6)
     chunk_size = trial.suggest_int("chunk_size", 200, 1200)
     if chunk_size < 400:
-        max_chunk_overlap = chunk_size
+        max_chunk_overlap = chunk_size - 1
     else:
-        max_chunk_overlap = 400
+        max_chunk_overlap = 500
 
     chunk_overlap = trial.suggest_int("chunk_overlap", 0, max_chunk_overlap)
-    bm25_k = trial.suggest_int("bm25_k", 1, 20)
+    bm25_k = trial.suggest_int("bm25_k", 1, 25)
+    bm25_weight = trial.suggest_float("bm25_weight", 0.0, 1.0)
+
+    rag_service: RagService = RagService(
+        nr_md_splitts = nr_md_splitts,
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap,
+        bm25_k = bm25_k,
+        bm25_weight = bm25_weight)
+    
+    context_recall, context_precision, context_entity_recall = rag_service.rag_evaluator()
+     
+    del rag_service
+    print(f"Context recall: {context_recall}")
+
+    return context_recall, context_precision
+
+
+
+
+def objective(trial: optuna.Trial) -> float:
+    nr_md_splitts = trial.suggest_int("md_splits", 1, 6)
+    chunk_size = trial.suggest_int("chunk_size", 200, 1200)
+    if chunk_size < 500:
+        max_chunk_overlap = chunk_size - 1
+    else:
+        max_chunk_overlap = 500
+
+    chunk_overlap = trial.suggest_int("chunk_overlap", 0, max_chunk_overlap)
+    bm25_k = trial.suggest_int("bm25_k", 1, 25)
     bm25_weight = trial.suggest_float("bm25_weight", 0.0, 1.0)
 
     rag_service: RagService = RagService(
@@ -150,24 +182,58 @@ def objective(trial: optuna.Trial) -> float:
     return context_recall, context_precision, context_entity_recall
 
 if __name__ == "__main__":
-    #storage = optuna.storages.InMemoryStorage()
+    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
+    study_name = "rag_builder_Parent_BM25_without_CER" 
+    storage_name = "sqlite:///{}.db".format(study_name)
 
-    file_path = "knowledge_base/optuna_data/optuna_journal_storage.log"
-    lock_obj = optuna.storages.journal.JournalFileOpenLock(file_path)
+    # file_path = "knowledge_base/optuna_data/optuna_journal_storage.log"
+    # lock_obj = optuna.storages.journal.JournalFileOpenLock(file_path)
 
-    storage = optuna.storages.JournalStorage(
-        optuna.storages.journal.JournalFileBackend(file_path, lock_obj=lock_obj),
-    )
+    # storage = optuna.storages.JournalStorage(
+    #     optuna.storages.journal.JournalFileBackend(file_path, lock_obj=lock_obj),
+    # )
 
     module = optunahub.load_module(package="samplers/auto_sampler")
     study = optuna.create_study(
+        study_name=study_name,
         sampler=module.AutoSampler(),
-        storage=storage,
-        directions=[StudyDirection.MAXIMIZE, StudyDirection.MAXIMIZE, StudyDirection.MAXIMIZE]
+        storage=storage_name,
+        load_if_exists= True,
+        directions=[StudyDirection.MAXIMIZE, StudyDirection.MAXIMIZE]
         )
-    study.optimize(objective, n_trials=20)
-    run_server(storage)
+    # run_server(storage_name)
+    # for _ in range(10):
+    #     study.optimize(objective2, n_trials=10)
 
-    #faith = test_faithfullness()
-    #print(RagService().rag_evaluator())
+    study_name = "rag_builder_Parent_BM25"
+    directions = [StudyDirection.MAXIMIZE, StudyDirection.MAXIMIZE, StudyDirection.MAXIMIZE]
+    storage_name = "sqlite:///{}.db".format(study_name)
+
+    study = optuna.create_study(
+        study_name=study_name,
+        sampler=module.AutoSampler(),
+        storage=storage_name,
+        load_if_exists= True,
+        directions=directions
+        )
+    
+    study.enqueue_trial({
+        "md_splits": 5,
+        "chunk_size": 1154,
+        "chunk_overlap": 231,
+        "bm25_k": 11,
+        "bm25_weight": 0.0895112
+    })
+    # for _ in range(1):
+    #      study.optimize(objective, n_trials=3)
+    #run_server(storage_name)
+
+    rag_service: RagService = RagService(
+        nr_md_splitts = 2,
+        chunk_size = 639,
+        chunk_overlap = 88,
+        bm25_k = 24,
+        bm25_weight = 0.6948053)
+
+    print(rag_service.rag_evaluator())
     #print(faith)
