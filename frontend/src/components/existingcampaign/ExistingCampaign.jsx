00df72b0bd3f9e3d89e4749d1977f374c3fbf991
SverreNystad <sverrenystad@gmail.com>
2025-02-20 21:03:25 +0100

    feat: add tailwind frontend

--- frontend/src/components/existingcampaign/ExistingCampaign.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ExistingCampaign() {
-  return <h2>Continue Campaign</h2>;
-}
-
-export default ExistingCampaign;
--- frontend/src/components/ruleslookup/RulesLookup.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function RulesLookup() {
-  return <h2>Rules Lookup</h2>;
-}
-
-export default RulesLookup;
