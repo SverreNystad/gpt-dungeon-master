eccfcb4057350494d0020480c2ecc343f457319c
Jon Bergland <jobergl@stud.ntnu.no>
2025-02-26 21:16:08 +0100

    refactor: remove unused file

--- rules-engine/src/knowledge_base/tools/tools.py
+++
@@ -1,8 +0,0 @@
-from knowledge_base.tools.example_tool import example_tool
-from langchain_core.tools import StructuredTool
-
-
-def get_tools() -> list[StructuredTool]:
-    tools = []
-    tools.append(example_tool)
-    return tools
