98b4e75bf11424469da444f0fc132c1a0801cd90
Jon Bergland <jobergl@stud.ntnu.no>
2025-03-07 00:09:24 +0100

    build: add capacitor config

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'no.gptdungeonmaster',
+  appName: 'GPT Dungeon Master',
+  webDir: 'dist'
+};
+
+export default config;
